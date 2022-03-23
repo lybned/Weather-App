5e28c090091d5a5c1356fa2c03799c43cec0598e
lybned <60757167+lybned@users.noreply.github.com>
2022-03-22 22:58:56 -0600

    Updated App.css and App.js
    Added weather for future 3 days under the location for today's weather.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
 
   const d = new Date();
-  const date = (d.getMonth()+1) + "/" + d.getDate() + "/" + d.getFullYear();
+  const datelist = [(d.getMonth()+1) + "/" + d.getDate() + "/" + d.getFullYear(), 
+                    (d.getMonth()+1) + "/" + (d.getDate()+1) + "/" + d.getFullYear(),
+                    (d.getMonth()+1) + "/" + (d.getDate()+2) + "/" + d.getFullYear(),
+                    (d.getMonth()+1) + "/" + (d.getDate()+3) + "/" + d.getFullYear(),
+                    (d.getMonth()+1) + "/" + (d.getDate()+4) + "/" + d.getFullYear()];
   return (
     <div>
       <body>
@@ -14,10 +18,15 @@ function App() {
 
         <div className="box">
           <div className="CityName">Calgary, Alberta</div>
-          <div className="Date">{date}</div>
+          <div className="Date">{datelist[0]}</div>
           <div className="Temperature">23&deg;C</div>
           <div className="Weather">Sunny</div>
-
+          
+          <div className="future">
+            <div className="futureWeather"><h3>{datelist[1]}</h3> <img className="WeatherIcon" src={logo} alt="Weather Icon"/>    <h7>20&deg;C</h7></div>
+            <div className="futureWeather"><h3>{datelist[2]}</h3> <img className="WeatherIcon" src={logo} alt="Weather Icon"/>    <h7>20&deg;C</h7></div>
+            <div className="futureWeather"><h3>{datelist[3]}</h3> <img className="WeatherIcon" src={logo} alt="Weather Icon"/>    <h7>20&deg;C</h7></div>
+          </div>
         </div>
       </body>
     </div>
